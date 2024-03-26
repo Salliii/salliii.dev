93d76143a43f94faebd111e139410b4cefa95ce3
Silas Schulreich <silas.schulreich@gmail.com>
2024-03-26 23:47:12 +0100

    FEATURE: add header component to root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import {fontPassionOne, fontPoetsenOne} from "@/public/fonts/fonts";
 import "@/styles/globals.css";
+import HeaderComponent from "@/components/header.component";
 
 export default function RootLayout({
 	children,
@@ -11,6 +12,12 @@ export default function RootLayout({
 		<html className={`${fontPoetsenOne.variable} ${fontPassionOne.variable}`}
 			lang={"en"}>
 			<body className={`${fontPassionOne.className}`}>
+				<HeaderComponent links={[
+					{name: "Home", href: "/"},
+					{name: "About", href: "/#about"},
+					{name: "Projects", href: "/#projects"},
+					{name: "Contact", href: "/#contact"},
+				]} />
 				{children}
 			</body>
 		</html>
