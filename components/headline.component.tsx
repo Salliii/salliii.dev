e9381fe8ce95efce1a4c9a562054bfdb6a37977a
Silas Schulreich <silas.schulreich@gmail.com>
2024-03-27 11:46:02 +0100

    FEATURE: add headline component

---
+++ components/headline.component.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import React, {HTMLAttributes} from "react";
+
+export type HeadlineComponentProps = {
+	children?: React.ReactNode,
+} & HTMLAttributes<HTMLDivElement>
+
+export function HeadlineComponent(props: HeadlineComponentProps) {
+	return (
+		<div {...props} className={`${props.className} flex flex-row`}>
+			<span className={"text-text-dimmed select-none"}>
+				&lt;
+			</span>
+			<div>
+				{props.children}
+				<span className={"text-text-dimmed select-none"}>
+					&gt;
+				</span>
+			</div>
+		</div>
+	);
+}
